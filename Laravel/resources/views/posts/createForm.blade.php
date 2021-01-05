ccc1d4e1f751bedb42e42ca801f6eb2c627f0968
IHprogram <ihm20202020@gmail.com>
2021-01-06 00:19:34 +0900

    投稿画面のコードを微修正

--- Laravel/resources/views/posts/createForm.blade.php
+++ Laravel/resources/views/posts/createForm.blade.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html>
+
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel='stylesheet' href='/css/app.css'>
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
 
   <body>
+
     <header>
-      <h1 class="page-header">Laravelを使った投稿機能の実装</h1>
+      <h1 class='page-header'>Laravelを使った投稿機能の実装</h1>
     </header>
-    <div class="container">
-      <h2 class="page-header">新しく投稿をする</h2>
+    <div class='container'>
+      <h2 class='page-header'>新しく投稿をする</h2>
       {!! Form::open(['url' => 'create']) !!}
-      <div class="form-group">
-        {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容']) !!}
-      </div>
-      <button type="submit" class="btn btn-success pull-right">追加</button>
+        <div class="form-group">
+          {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容内容']) !!}
+        </div>
+        <button type="submit" class="btn btn-success pull-right">追加</button>
       {!! Form::close() !!}
     </div>
     <footer>
@@ -27,4 +28,5 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
   </body>
+
 </html>
