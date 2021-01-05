ca21308ce901e82d687e1be71a0c52c950eb16fe
IHprogram <ihm20202020@gmail.com>
2021-01-06 00:41:45 +0900

    登録処理だけを行うcreateメソッドの実装

--- Laravel/app/Http/Controllers/PostsController.php
+++ Laravel/app/Http/Controllers/PostsController.php
@@ -25,4 +25,20 @@ class PostsController extends Controller
     {
         return view('posts.createForm');
     }
+
+    // 以下のcreateメソッドは、ブラウザに表示されず、登録処理だけを行うメソッド。
+    // POST通信で、フォームから値が送られる場合は、
+    // 引数に用意してある$request変数の中に値(パラメータ)が渡されることになる
+    public function create(Request $request)
+    {
+        // その中から、<input>タグのname属性が「newPost」
+        // と指定されていたところの値を$post変数内に入れている
+        $post = $request->input('newPost');
+        // あとは、postsテーブルのpostカラムに、$post変数を当てはめて登録を行っている
+        DB::table('posts')->insert([
+            'post' => $post
+        ]);
+
+        return redirect('/index');
+    }
 }
