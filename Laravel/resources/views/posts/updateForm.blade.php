38fc3afc49bf7563f7a7ee9f2cb48c0c08cfcd2e
IHprogram <ihm20202020@gmail.com>
2021-01-09 23:42:43 +0900

    各ファイルの継承先を変更
    各ファイルがlayoutsディレクトリ内のapp.blade.phpを継承するよう変更した。

--- Laravel/resources/views/posts/updateForm.blade.php
+++ Laravel/resources/views/posts/updateForm.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel='stylesheet' href='/css/app.css'>
-  <title>Document</title>
-</head>
-<body>
-  <head>
-    <h1 class='page-header'>Laravelを使った更新機能の実装</h1>
-  </head>
+@extends('layouts.app')
+
+@section('content')
   <div class='container'>
     <h2 class='page-header'>投稿内容を変更する</h2>
     {!! Form::open(['url' => '/post/update']) !!}
@@ -20,10 +11,4 @@
       <button type="submit" class="btn btn-primary pull-right">更新</button>
     {!! Form::close() !!}
   </div>
-  <footer>
-    <small>Laravel@dawn.curriculum</small>
-  </footer>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-</body>
-</html>
+@endsection
